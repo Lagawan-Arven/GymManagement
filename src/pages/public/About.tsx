a6ce94d150e26c2a9bede992a13644682f32e670
Arven Lagawan <arvenlagawan0731@gmail.com>
2026-02-03 20:25:35 +0800

    added public pages

---
+++ src/pages/public/About.tsx
@@ -0,0 +1,10 @@
+function About() {
+  return (
+    <div className="bg-gray-500">
+      <h1>About Us</h1>
+      <p>This is the About page of the Gym Management System.</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/pages/public/Landing.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <div className="bg-gray-500">
+      <h1>Landing</h1>
+    </div>
+  );
+};
+
+export default Landing;
